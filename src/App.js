721bc9ecce44e5c018a01c19b49c8eedbd1f91e6
Katakung95 <tanwasripan@hotmail.co.uk>
2022-02-25 15:19:55 +0000

    completed layout and included sound functionality when clock hits 00:00

--- src/App.js
+++ src/App.js
@@ -12,18 +12,28 @@ class App extends React.Component {
       curTimer: "Session",
       timeLeft: "25:00",
       start: true,
-      intervalId: undefined,
+      intervalId: undefined
     }
     this.changeLength = this.changeLength.bind(this);
     this.start = this.start.bind(this);
     this.reset = this.reset.bind(this);
     this.ticking = this.ticking.bind(this);
+    this.playSound = this.playSound.bind(this);
+  }
+  
+  // select the audio element when component is mounted
+  componentDidMount() {
+    this.setState({
+      audio: document.getElementById("beep"),
+    });
   }
 
   changeLength(e) {
     const btnClick = e.target.id;
+    // implementing increase and decrease of length of session and break
     switch(btnClick) {
       case "break-increment":
+        // max num and min num condition checks
         if (this.state.break >= 60) return;
         this.setState({
           break: this.state.break + 1,
@@ -59,35 +69,37 @@ class App extends React.Component {
   }
 
   start() {
+    // set start state - toggling
     this.setState({
       start: !this.state.start,
-    })
-    console.log(this.state.start);
+    });
+    // selecting, enabling and disabling increment and decrement button while clock is ticking
     const controlBtns = document.getElementsByClassName('control-btn');
     if (this.state.start) {
       controlBtns.forEach(btn => btn.disabled = true);
     } else {
       controlBtns.forEach(btn => btn.disabled = false);
     }
+    // if state is start when button was clicked, start the clock
     if (this.state.start) {
-      console.log("start");
       this.ticking();
       this.intervalId = setInterval(this.ticking, 1000);
     } else {
-      console.log("pause")
       clearInterval(this.intervalId);
     }
   }
 
   ticking() {
-    let timer;
+    // switching between session and break
     if (this.state.curTimer === "Session" && this.state.timer < 0) {
+      this.playSound();
       this.setState({
         timer: this.state.break * 60,
         curTimer: "Break",
       });
     }
     if (this.state.curTimer === "Break" && this.state.timer < 0) {
+      this.playSound();
       this.setState({
         timer: this.state.session * 60,
         curTimer: "Session",
@@ -108,7 +120,7 @@ class App extends React.Component {
   reset() {
     // if interval exists clear it
     if (this.intervalId) clearInterval(this.intervalId);
-    // make all buttons works
+    // return functionality for control buttons when reset
     const controlBtns = document.getElementsByClassName('control-btn');
     controlBtns.forEach(btn => btn.disabled = false);
     // reset all states
@@ -120,7 +132,26 @@ class App extends React.Component {
       timeLeft: "25:00",
       start: true,
       intervalId: undefined,
-    })
+    });
+    // reset audio
+    try {
+      this.state.audio.pause();
+      this.state.audio.currentTime = 0;
+    } 
+    catch (error) {
+      console.log("this is the error: "+ error);
+    }
+    
+  }
+
+  playSound() {
+    // play the audio element
+    this.state.audio.play();
+    // stop sound after 2.5 seconds
+    setTimeout(()=>{
+      this.state.audio.pause();
+      this.state.audio.currentTime = 0;
+    }, 2500);
   }
 
   render() {
@@ -139,8 +170,11 @@ class App extends React.Component {
             curTimer={this.state.curTimer}
             timeLeft={this.state.timeLeft}
           />
-          <button id="start_stop" onClick={this.start}>{this.state.start ? "Play": "Pause"}</button>
-          <button id="reset" onClick={this.reset}>Reset</button>
+          <section className="bottom-controls">
+            <button id="start_stop" onClick={this.start}>{this.state.start ? "Play": "Pause"}</button>
+            <button id="reset" onClick={this.reset}>Reset</button>
+          </section>
+          <audio volume="0.5" id="beep" src="https://www.fesliyanstudios.com/play-mp3/4384"></audio>
         </main>
         <footer> Coded by Tanwa Sripan </footer>
       </>
--- src/components/Control.js
+++ src/components/Control.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 export default function Control(props) {
     return (
-        <section>
-            <div>
+        <section className="top-controls">
+            <div className="break-container">
                 <p id="break-label">Break Length</p>
-                <button id="break-increment" className="control-btn" onClick={props.onClick}>&#8593;</button>
+                <button id="break-increment" className="control-btn" onClick={props.onClick}><i className="fas fa-angle-up fa-lg"></i></button>
                 <p id="break-length">{props.break}</p>
-                <button id="break-decrement" className="control-btn" onClick={props.onClick}>&#8595;</button>
+                <button id="break-decrement" className="control-btn" onClick={props.onClick}><i className="fas fa-angle-down fa-lg"></i></button>
             </div>
-            <div>
+            <div className="session-container">
                 <p id="session-label">Session Length</p>
-                <button id="session-increment" className="control-btn" onClick={props.onClick}>&#8593;</button>
+                <button id="session-increment" className="control-btn" onClick={props.onClick}><i className="fas fa-angle-up fa-lg"></i></button>
                 <p id="session-length">{props.session}</p>
-                <button id="session-decrement" className="control-btn" onClick={props.onClick}>&#8595;</button>
+                <button id="session-decrement" className="control-btn" onClick={props.onClick}><i className="fas fa-angle-down fa-lg"></i></button>
             </div>
         </section>
     )
